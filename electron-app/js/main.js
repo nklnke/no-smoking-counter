6212fdff40664772b11f2f4df9e3b9a7df0a5ec6
Danil Kozlov <megacokes@gmail.com>
2019-05-14 11:07:45 +0500

    cleanin'n'fixin

--- electron-app/js/main.js
+++ electron-app/js/main.js
@@ -9,7 +9,7 @@ function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 550,
-    height: 585,
+    height: 570,
     webPreferences: {
       nodeIntegration: true
     },
